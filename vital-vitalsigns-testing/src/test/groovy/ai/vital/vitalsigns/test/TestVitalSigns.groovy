ac1c1ee41c2321be2eaadbb7f60ef9598dd070ec
Marc Hadfield <marc@vital.ai>
2024-01-31 13:32:33 -0500

    sync

---
+++ vital-vitalsigns-testing/src/test/groovy/ai/vital/vitalsigns/test/TestVitalSigns.groovy
@@ -0,0 +1,26 @@
+package ai.vital.vitalsigns.test
+
+import ai.vital.vitalsigns.VitalSigns
+
+class TestVitalSigns {
+
+    static void main(String[] args) {
+
+        TestVitalSigns test = new TestVitalSigns()
+
+        test.runTest(args)
+    }
+
+    public runTest(args) {
+
+        println "Hello World!"
+
+        VitalSigns vs = VitalSigns.get()
+
+
+
+
+    }
+
+
+}
