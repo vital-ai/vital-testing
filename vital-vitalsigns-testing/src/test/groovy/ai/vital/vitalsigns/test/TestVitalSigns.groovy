50bcd324329eaac7fdcccf2c3cec6f0c6c03fcbf
Marc Hadfield <marc@vital.ai>
2024-02-01 18:46:27 -0500

    sync

--- vital-vitalsigns-testing/src/test/groovy/ai/vital/vitalsigns/test/TestVitalSigns.groovy
+++ vital-vitalsigns-testing/src/test/groovy/ai/vital/vitalsigns/test/TestVitalSigns.groovy
@@ -1,6 +1,9 @@
 package ai.vital.vitalsigns.test
 
+import ai.vital.domain.Thing
 import ai.vital.vitalsigns.VitalSigns
+import ai.vital.vitalsigns.model.VITAL_Node
+import ai.vital.vitalsigns.model.VitalApp
 
 class TestVitalSigns {
 
@@ -17,10 +20,14 @@ class TestVitalSigns {
 
         VitalSigns vs = VitalSigns.get()
 
+        VitalApp app = VitalApp.withId("vitalapp")
 
+        Thing thing = new Thing().generateURI(app)
 
+        thing.name = "The Thing"
+
+        println "Thing: " + thing.toJSON()
 
     }
-
 
 }
